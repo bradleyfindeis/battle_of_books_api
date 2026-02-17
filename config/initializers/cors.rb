b906cebb1f9d7d1aada50f15e34a6cdde8437d80
Bradley Findeis <bradleyfindeis@gmail.com>
2026-02-16 22:10:54 -0700

    fixing cors errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins ENV.fetch('FRONTEND_URL', 'https://battle-of-books.onrender.com')
+      origins ENV.fetch('FRONTEND_URL', 'https://battle-of-books-frontend.onrender.com')
     else
       origins 'http://localhost:5173', 'http://localhost:3001', 'http://127.0.0.1:5173'
     end
